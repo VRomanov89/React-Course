75074522af16f765638b5b232b1bc1744b48967e
Vladimir Romanov <vladromweb@gmail.com>
2022-04-30 21:06:53 -0400

    Finalized Item CSS Styling
    Finalized Item CSS Styling

--- 02. Starting Project/01-starting-setup/src/components/ExpenseItem.js
+++ 02. Starting Project/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <div>Date</div>;
+  return (
+    <div className='expense-item'>
+      <div>Date</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$456.34</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
