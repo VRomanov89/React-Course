4a36f0c3acd570734a9a732196bac38c15c60d67
Vladimir Romanov <vladromweb@gmail.com>
2022-04-30 21:49:20 -0400

    Elements with imports and props
    Elements with imports and props

--- 02. Starting Project/01-starting-setup/src/components/ExpenseItem.js
+++ 02. Starting Project/01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
-    <div className='expense-item'>
-      <div>Date</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$456.34</div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
